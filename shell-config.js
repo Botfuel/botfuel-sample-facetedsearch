d0d04b4de370e47484526f6cba1dbc0be41c29ff
Yan Georget <yan@botfuel.io>
2018-04-09 18:45:30 +0200

    fixed corpora which where causing corpus extractor to loop

--- shell-config.js
+++ shell-config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  locale: 'en',
-  adapter: {
-    name: 'shell',
-  },
-  brain: {
-    name: 'memory',
-  },
   modules: ['botfuel-module-facetedsearch'],
   path: __dirname,
 };
