8818cd78cf75bc94e0923046f29cf4434355420b
Canh <canh85@gmail.com>
2018-04-11 18:04:15 +0200

    fix: jean ==> jeans

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -7,7 +7,7 @@ describe('test bot scenario', () => {
   test('Bot return articles without asking unneccessary further question ', async () => {
     const bot = new Bot(config);
     const { userId } = bot.adapter;
-    await bot.play([new UserTextMessage('I want to buy Levis jeans')]);
+    await bot.play([new UserTextMessage('I want to buy some Levis jeans')]);
     expect(bot.adapter.log).toContainEqual(new BotTextMessage('Thank you. We have 2 products:').toJson(userId));
   });
 
